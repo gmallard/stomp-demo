6098887918b7b7e113b78dc8505a96479a4ed99d
Guy M. Allard <allard.guy.m@gmail.com>
2009-08-22 14:32:49 -0400

    Add initial message put and get example.

---
+++ queue_get.rb
@@ -0,0 +1,15 @@
+require 'rubygems'
+require 'stomp'
+client = Stomp::Client.open "login", "passcode", "localhost", 51613
+
+client.subscribe("/queue/test", {
+                "persistent" => true,
+                "client-id" => "rubyClient",
+        } ) do |message|
+  puts "Got Reply: ID=#{message.headers['message-id']} BODY=#{message.body} on #{message.headers['destination']}"
+end
+
+
+gets
+client.close
+
