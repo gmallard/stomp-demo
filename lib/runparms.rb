4c7876482e941959a7aee03e824cbb2b2885a9c4
Guy M. Allard <allard.guy.m@gmail.com>
2009-08-31 12:42:00 -0400

    Allow server connect parameter overrides from YAML properties file.

--- lib/runparms.rb
+++ lib/runparms.rb
@@ -1,5 +1,6 @@
 #
 require 'logger'
+require 'yaml'
 #
 class Runparms
   #
@@ -11,10 +12,38 @@ class Runparms
     @@log = Logger.new(STDOUT)
     @@log.level = Logger::DEBUG
     #
-    @userid = params[:userid] ? params[:userid] : "login"
-    @password = params[:password] ? params[:password] : "passcode"
-    @host = params[:host] ? params[:host] : "localhost"
-    @port = params[:port] ? params[:port] : 51613
+    @userid, @password, @host, @port = nil, nil, nil, nil
+    #
+    # Properties from YAML file first.
+    #
+    yfname = File.join(File.dirname(__FILE__), "..", "props.yaml")
+    @@log.debug "YAML Parms File Name: #{yfname}"
+    yaml_params = nil
+    if File.exists?(yfname)
+      @@log.debug "Exists!"
+      yaml_parms = YAML.load(File.open(yfname))
+      @@log.debug "YAML Parms: #{yaml_parms.inspect}"
+      #
+      @userid = yaml_parms[:user_id]
+      @password = yaml_parms[:password]
+      @host = yaml_parms[:host]
+      @port = yaml_parms[:port]
+    end
+    #
+    #
+    # Override with CTOR parms.
+    #
+    @userid = params[:userid] if params[:userid]
+    @password = params[:password] if params[:password]
+    @host = params[:host] if params[:host]
+    @port = params[:port] if params[:port]
+    #
+    # Last resort is hard coded values here.
+    #
+    @userid = "login" if not @userid
+    @password = "passcode" if not @password
+    @host = "localhost" if not @host
+    @port = 51613 if not @port
   end
   #
   def to_s
