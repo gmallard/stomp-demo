b824fcc418853333f8ae054dea7faa9ae9d58d5f
Guy M. Allard <allard.guy.m@gmail.com>
2009-09-01 09:29:01 -0400

    Paramaterize threaded example.

--- threaded/getters-demo.rb
+++ threaded/getters-demo.rb
@@ -1,54 +1,95 @@
 require 'rubygems'
 require 'stomp'
+require 'logger'
+$:.unshift File.join(File.dirname(__FILE__), "..", "lib")
+require 'runparms'
 #
-MAX_GETTERS = 2 - 1
-MAX_WKE = 4 - 1
-#
-puts "Max Gettters: #{MAX_GETTERS + 1}"
-puts "Max WKEs: #{MAX_WKE + 1}"
-#
-getters = (0..MAX_GETTERS).map do |i|
-  Thread.new("consumer #{i}", i) do |name, getter_num|
-    client = Stomp::Client.open "login", "passcode", "localhost", 51613
-    received = nil
-    queue_name = "/queue/test#{getter_num}"
-    client_name = "rubyClient#{getter_num}"
-    client.subscribe(queue_name, {
-                    "persistent" => true,
-                    "client-id" => client_name,
-            } ) do |message|
-      # puts "#{p message}"
-      puts "#{name} Got Reply: BODY=#{message.body} on #{message.headers['destination']}"
-      received = message
+class ThreadedGetters
+  #
+  attr_reader :getters
+  #
+  def initialize(params = {})
+    @@log = Logger.new(STDOUT)
+    @@log.level = Logger::DEBUG
+    #
+    @queue_name_base = params[:queue_name_base] ? 
+      params[:queue_name_base] : "/queue/testbasic"
+    @max_getters = params[:max_getters] ? 
+      params[:max_getters] : 2 - 1
+    @max_wke = params[:max_wke] ? 
+      params[:max_wke] : 2 - 1
+    #
+    @getters = nil
+    @runparms = Runparms.new
+    #
+    @@log.debug "Max Gettters: #{@max_getters + 1}"
+    @@log.debug "Max WKEs: #{@max_wke + 1}"
+  end
+  #
+  def start_getters
+    #
+    @getters = (0..@max_getters).map do |i|
+      Thread.new("consumer #{i}", i) do |name, getter_num|
+        client = Stomp::Client.open(@runparms.userid, @runparms.password, 
+          @runparms.host, @runparms.port)
+        received = nil
+        client_name = "GetterClient#{getter_num}"
+        client.subscribe(queue_name(getter_num), {
+                        "persistent" => true,
+                        "client-id" => client_name,
+                } ) do |message|
+          # @@log.debug "#{p message}"
+          lmsg = "#{name} Got Reply: BODY=#{message.body} "
+          lmsg += "on QUEUE #{message.headers['destination']}"
+          @@log.debug "#{lmsg}"
+          proc_message(message)
+          received = message
+        end
+        sleep 0.5 until received
+        client.close
+        @@log.debug "getter #{name} ending"
+      end
     end
-    sleep 0.5 until received
+  end
+  #
+  def run_putter
+    client = Stomp::Client.open(@runparms.userid, @runparms.password, 
+      @runparms.host, @runparms.port)
+    @@log.debug "starting puts"
+    next_queue_num = @max_getters
+    for i in 0..@max_wke do
+       next_queue_num += 1
+       next_queue_num = 0 if next_queue_num > @max_getters
+       run_queue_name = queue_name(next_queue_num)
+       client_name = "PutterClient#{next_queue_num}"
+       message = "WKE Num: #{i} to queue #{run_queue_name}"
+       @@log.debug "Putting: #{message}"
+       client.send(run_queue_name, message, {
+        "persistent" => true,
+        "client-id" => client_name,
+        "reply-to" => run_queue_name,
+        }
+      )
+    end
+    @@log.debug "ending puts"
     client.close
-    puts "getter #{name} ending"
+  end
+  #
+  private
+  def proc_message(m)
+    @@log.debug "Processing: #{m.body}"
+  end
+  #
+  def queue_name(nxtn)
+    "#{@queue_name_base}#{nxtn}"
   end
 end
 #
-client = Stomp::Client.open "login", "passcode", "localhost", 51613
-puts "starting puts"
-next_queue_num = MAX_GETTERS
-for i in 0..MAX_WKE do
-   next_queue_num += 1
-   next_queue_num = 0 if next_queue_num > MAX_GETTERS
-   queue_name = "/queue/test#{next_queue_num}"
-   client_name = "Client#{next_queue_num}"
-   message = "WKE Num: #{i} to queue #{queue_name}"
-   puts "Putting: #{message}"
-   client.send(queue_name, message, {
-    "persistent" => true,
-    "client-id" => client_name,
-    "reply-to" => queue_name,
-    }
-  )
-end
-puts "ending puts"
-client.close
-#
+threader = ThreadedGetters.new
+threader.run_putter
+puts "Starting putter"
+threader.start_getters
 puts "Starting getter wait"
-getters.each {|th| th.join}
+threader.getters.each {|th| th.join}
 puts "Complete"
-#
 
