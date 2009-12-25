0a7115cc7dee898a60003f186b70a7c0d14c56c1
Guy M. Allard <allard.guy.m@gmail.com>
2009-12-25 14:14:56 -0500

    Clarify the simple eventmachine client example

--- em/connect/connect.rb
+++ em/connect/connect.rb
@@ -3,13 +3,12 @@ require 'logger'
 require 'rubygems'
 require 'eventmachine'
 #
-# This example is pretty much striaght from the eventmachine documentation.
+# This example is almost like the eventmachine documentation.
+#
 # Connects were made to both:
 #
 # * stompserver on port 51613
 # * AMQ on port 61613
-#
-# The topic/queue name was changed so that AMQ connect matches what it supports.
 #
 module StompClient
   include EM::Protocols::Stomp
@@ -18,6 +17,7 @@ module StompClient
     super
     @log = Logger::new(STDOUT)
     @log.level = Logger::DEBUG
+    @conn_headers = {:login => "guest", :passcode => "guestpass"}
   end
   #
   def post_init
@@ -25,32 +25,45 @@ module StompClient
   end
   #
   def connection_completed
-    connect :login => 'guest', :passcode => 'guest'
+    connect @conn_headers
     @log.debug("connection_completed done")
-  end
-  #
-  def subscribe(dest,ack=false)
-    super(dest,ack)
-    @log.debug("subscribe done")
   end
   #
   def receive_msg(msg)
     if msg.command == "CONNECTED"
       @log.debug("received CONNECTED")
-      subscribe '/temp-queue/abcd123'
     else
-      p ['got a message', msg]
-      @log.debug(msg.body)
+      @@log.debug("got a message: #{msg.inspect}")
     end
+  end
+  #
+  def set_connection_headers(params = {})
+    @conn_headers.merge!(params)
   end
 end
 #
 EM.run {
   puts "EM.run starts"
-  # stompserver
-  EM.connect('localhost', 51613, StompClient)
-  # AMQ
-  # EM.connect('localhost', 61613, StompClient)
+  #
+  port = ENV['STOMP_PORT'] ? ENV['STOMP_PORT'] : 51613
+  host = ENV['STOMP_HOST'] ? ENV['STOMP_HOST'] : "localhost"
+  #
+  conn = EM.connect(host, port, StompClient)
+  puts "EM.run Connection complete to #{host} on port #{port}"
+  # Override connection parameters.  (Is there another way to do this?)
+  conn.set_connection_headers(:login => "gmallard", :passcode => "bigguy" )
+  #
+  # Send stomp DISCONNECT frame after 10 seconds.
+  #
+  EventMachine::add_timer( 10 ) {
+    #
+    # send_data(data) takes a well-formed stomp frame!
+    #
+    conn.send_data("DISCONNECT\n\n\x00")
+    puts "EM.run disconnect sent"
+    #
+    EventMachine::stop_event_loop()
+  }
   puts "EM.run done"
 }
 
