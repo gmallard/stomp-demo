8a876277e88cc81c8cdf4fbd00c220dc9404e678
gmallard <allard.guy.m@gmail.com>
2010-05-16 08:27:40 -0400

    Generalize and document the thread hang example.

--- threaded-two/hanger.rb
+++ threaded-two/hanger.rb
@@ -1,28 +1,52 @@
 #!/usr/bin/ruby
-
+#
+# = Hanger
+#
+# A demonstration program known to 'hang' with various levels of the 
+# Ruby stomp client gem.  This code will complete normally with:
+#
+# * stomp 1.1.1
+# * stomp 1.1.5 + fixes
+#
+# Intermediate releases will hang attempting to run client code like this.
+#
 require 'rubygems'
 require 'stomp'
 require 'thread'
-
-@conn = Stomp::Connection.open("rip", "foo", "oldpete", 51613, true)
-
-
-puts("Starting thread to poll the socket")
-Thread.new(@conn) do |amq|
+require 'logger'
+$:.unshift File.join(File.dirname(__FILE__), "..", "lib")
+require 'runparms'
+require 'stomphelper'
+#
+log = Logger.new(STDOUT)
+log.level = Logger::DEBUG
+#
+log.debug("Main: #{Thread.current}")
+#
+runparms = Runparms.new
+conn = Stomp::Connection.open(runparms.userid, runparms.password,
+  runparms.host, runparms.port, true)
+#
+log.debug("Connection is: #{conn.inspect}")
+log.debug("Starting thread to receive from the socket")
+#
+Thread.new(conn) do |amq|
+    log.debug "Receiver: #{Thread.current}"
     while true
         msg = amq.receive
+        log.debug "receive done"
         dest = msg.headers["destination"]
         time = Time.now.strftime('%H:%M:%S')
-
-        puts "\r#{time}:#{dest} > #{msg.body.chomp}\n"
+        log.debug "#{time}:#{dest} > #{msg.body.chomp}"
     end
 end
-
-puts("Subscribing to /topic/thread.test")
-@conn.subscribe("/topic/thread.test")
-
-puts("Sending to /topic/thread.test")
-@conn.send("/topic/thread.test", Time.now.to_s)
-
-puts("Sleeping 1 second")
+#
+log.debug("Subscribing to /topic/thread.test")
+conn.subscribe("/topic/thread.test")
+#
+log.debug("Sending to /topic/thread.test")
+conn.send("/topic/thread.test", Time.now.to_s)
+#
+log.debug("Sleeping 1 second")
 sleep 1
+
